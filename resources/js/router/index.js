8ec16928d0f58f690d62f5df22d92a95a84a74bf
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-20 10:05:04 +0100

    first commit

---
+++ resources/js/router/index.js
@@ -0,0 +1,12 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const routes = [
+    { path: '/', component: () => import('../components/Home.vue') },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
