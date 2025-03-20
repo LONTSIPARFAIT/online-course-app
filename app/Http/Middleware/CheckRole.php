11402720e61121bc6373664666bfff4b81160b43
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-20 10:51:22 +0100

    first commit

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -4,17 +4,15 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
 
 class CheckRole
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next, $role)
     {
+        if (!Auth::check() || Auth::user()->role !== $role) {
+            return redirect('/');
+        }
         return $next($request);
     }
 }
