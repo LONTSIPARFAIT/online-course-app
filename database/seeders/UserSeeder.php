a9671b42971635a484d00fb67631517e08c415c4
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-20 10:31:27 +0100

    first commit

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,16 +2,32 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        User::create([
+            'name' => 'Étudiant Test',
+            'email' => 'student@test.com',
+            'password' => bcrypt('password'),
+            'role' => 'student',
+        ]);
+
+        User::create([
+            'name' => 'Enseignant Test',
+            'email' => 'teacher@test.com',
+            'password' => bcrypt('password'),
+            'role' => 'teacher',
+        ]);
+
+        User::create([
+            'name' => 'Admin Test',
+            'email' => 'admin@test.com',
+            'password' => bcrypt('password'),
+            'role' => 'admin',
+        ]);
     }
 }
