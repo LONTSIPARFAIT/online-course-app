30daa50521de3ef9c51d3ac5fd7fae0f3a329925
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-20 10:37:15 +0100

    first commit

--- database/seeders/CourseSeeder.php
+++ database/seeders/CourseSeeder.php
@@ -2,16 +2,23 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Course;
 
 class CourseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        Course::create([
+            'title' => 'Introduction à Laravel',
+            'description' => 'Un cours pour apprendre les bases de Laravel.',
+            'teacher_id' => 2, // ID de l’enseignant (teacher@test.com)
+        ]);
+
+        Course::create([
+            'title' => 'Vue.js pour débutants',
+            'description' => 'Découvrez Vue.js avec ce cours pratique.',
+            'teacher_id' => 2,
+        ]);
     }
 }
