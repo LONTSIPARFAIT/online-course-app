25861a6aa23aaecc4f19016720a3dfae609512d6
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-20 10:48:56 +0100

    first commit

---
+++ resources/js/components/Teacher/Dashboard.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1>Tableau de bord enseignant</h1>
+        <p>Bienvenue, enseignant !</p>
+    </div>
+</template>
+<script>
+export default { name: 'TeacherDashboard' };
+</script>
