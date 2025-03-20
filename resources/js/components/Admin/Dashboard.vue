07abfe734f43c626ea5fdd21ef15679067c4facc
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-20 10:49:39 +0100

    first commit

---
+++ resources/js/components/Admin/Dashboard.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1>Tableau de bord administrateur</h1>
+        <p>Bienvenue, admin !</p>
+    </div>
+</template>
+<script>
+export default { name: 'AdminDashboard' };
+</script>
