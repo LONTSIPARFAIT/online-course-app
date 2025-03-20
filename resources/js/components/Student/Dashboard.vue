79d73fb25d3828f5601ea7b62e639d4128c9b42b
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-20 10:48:32 +0100

    first commit

---
+++ resources/js/components/Student/Dashboard.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1>Tableau de bord étudiant</h1>
+        <p>Bienvenue, étudiant !</p>
+    </div>
+</template>
+<script>
+export default { name: 'StudentDashboard' };
+</script>
