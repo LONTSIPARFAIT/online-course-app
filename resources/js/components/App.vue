cd5ccc1cedd1f4d9b112b199c0cee3ac26522acb
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-20 10:03:50 +0100

    first commit

---
+++ resources/js/components/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Plateforme de cours en ligne</h1>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+};
+</script>
