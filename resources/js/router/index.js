453cee1574c5f66ce09cd2cb42a6f660a6a14ca9
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-20 10:43:01 +0100

    first commit

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -2,6 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
     { path: '/', component: () => import('../components/Home.vue') },
+    { path: '/student/dashboard', component: () => import('../components/Student/Dashboard.vue') },
+    { path: '/teacher/dashboard', component: () => import('../components/Teacher/Dashboard.vue') },
+    { path: '/admin/dashboard', component: () => import('../components/Admin/Dashboard.vue') },
 ];
 
 const router = createRouter({
