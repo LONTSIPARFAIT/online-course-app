652198790e970b4a86389f5afe0b1edbe1d0936b
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-20 10:05:37 +0100

    first commit

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <p>Bienvenue sur la plateforme avec Laravel 12 et MySQL !</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+};
+</script>
