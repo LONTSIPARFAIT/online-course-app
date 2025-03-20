9d9a119a7205f46a37e2a7ec672be5b5927fa47b
LONTSIPARFAIT <lontsiparfait12@gmail.com>
2025-03-20 11:16:46 +0100

    first commit

--- routes/web.php
+++ routes/web.php
@@ -26,26 +26,42 @@ use Inertia\Inertia;
 
 // require __DIR__.'/auth.php';
 
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
 
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+// Routes personnalisées pour chaque rôle
 Route::middleware(['auth', 'role:student'])->group(function () {
     Route::get('/student/dashboard', function () {
-        return view('welcome');
-    });
+        return Inertia::render('Student/Dashboard');
+    })->name('student.dashboard');
 });
 
 Route::middleware(['auth', 'role:teacher'])->group(function () {
     Route::get('/teacher/dashboard', function () {
-        return view('welcome');
-    });
+        return Inertia::render('Teacher/Dashboard');
+    })->name('teacher.dashboard');
 });
 
 Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('/admin/dashboard', function () {
-        return view('welcome');
-    });
+        return Inertia::render('Admin/Dashboard');
+    })->name('admin.dashboard');
 });
 
-Route::get('/test', function () {
-    return "Test OK";
-})->middleware('role:student');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
